0c08b0d4690c123bf065ea4848eade89aa821506
TomiMMI <prot.tom91@gmail.com>
2024-01-07 10:14:16 -0500

    email system

---
+++ email.php
@@ -0,0 +1,34 @@
+<?php
+$destinataire = "prot.tom91@gmail.com";
+$sujet = '[Sans sujet]';
+$message =  "[Le contenu du message est vide]";
+
+$infos = "MIME-Version: 1.0" . "\r\n"; 
+$infos .= "charset=UTF-8" . "\r\n"; 
+
+if(!empty($_POST["sujet"])){
+    $sujet = $_POST["sujet"];
+}
+if(!empty($_POST["message"])){
+    $message = $_POST["message"];
+}
+if(!empty($_POST["prenom"]) || !empty($_POST["nom"])){
+    $infos .= "From: ".$_POST["prenom"]." ".$_POST["nom"]. "\r\n";
+}
+else {
+    $infos  .= "From: [Expediteur inconnu] ";
+}
+if(!empty($_POST["email"])){
+    $test = strval("<").strval($_POST["email"].strval(">"));
+}
+else {
+    $infos  .= " <adresseem@il.nonrenseignee>" . "\r\n";
+}
+if(mail($destinataire, $sujet, $message, $infos)){
+    $_SESSION["success"] = 1;
+    header("./refonte/index.html");
+} else{
+    $_SESSION["success"] = 0;
+    header("./refonte/index.html");
+}
+?>
